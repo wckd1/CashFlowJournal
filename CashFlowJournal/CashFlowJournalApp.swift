7aa3a7b34bb55595777edacfa91e27a6c24e14eb
wckd1 <korobeinikovr@gmail.com>
2023-12-28 12:27:12 +0100

    Added initial project

---
+++ CashFlowJournal/CashFlowJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlowJournalApp.swift
+//  CashFlowJournal
+//
+//  Created by Роман Коробейников on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CashFlowJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
