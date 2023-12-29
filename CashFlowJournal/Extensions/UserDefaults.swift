416f52b27f953464b5e1bae18a7b5a9fb9429e75
wckd1 <korobeinikovr@gmail.com>
2023-12-29 11:17:09 +0100

    Reorganized folders structure

--- CashFlowJournal/Extensions/UserDefaults.swift
+++ CashFlowJournal/Extensions/UserDefaults.swift
@@ -9,21 +9,24 @@ import Foundation
 
 extension UserDefaults {
     
+    static let usernameKey = "username"
+    static let isOnboardedKey = "isOnboarded"
+    
     static var username: String {
         get {
-            UserDefaults.standard.string(forKey: Constants.Preferences.USERNAME) ?? ""
+            UserDefaults.standard.string(forKey: UserDefaults.usernameKey) ?? ""
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: Constants.Preferences.USERNAME)
+            UserDefaults.standard.set(newValue, forKey: UserDefaults.usernameKey)
         }
     }
     
     static var isOnboarded: Bool {
         get {
-            UserDefaults.standard.bool(forKey: Constants.Preferences.IS_ONBOARDED)
+            UserDefaults.standard.bool(forKey: UserDefaults.isOnboardedKey)
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: Constants.Preferences.IS_ONBOARDED)
+            UserDefaults.standard.set(newValue, forKey: UserDefaults.isOnboardedKey)
         }
     }
 }
