eb0b7daf8d620b78b1c125fef169e31a9cfefccf
wckd1 <korobeinikovr@gmail.com>
2024-01-12 16:27:44 +0100

    Fixed icon picker's search bar compression

--- CashFlowJournal/IconPicker/IconPickerModalView.swift
+++ CashFlowJournal/IconPicker/IconPickerModalView.swift
@@ -18,38 +18,34 @@ struct IconPickerModalView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                ScrollView(.vertical) {
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))], spacing: 20) {
-                        ForEach(symbols.filter { searchText.isEmpty ? true : $0.contains(searchText.lowercased()) }, id: \.hash) { icon in
-                            
-                            Button {
-                                self.selection = icon
-                            } label: {
-                                Image(systemName: icon)
-                                    .font(.system(size: 25))
-                                    .foregroundColor(self.selection == icon ? Color.primary_color : Color.text_color)
-                            }
-                        }.padding(.top, 5)
-                    }
-                }
-                .navigationTitle("icon_picker_title")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .primaryAction) {
+            ScrollView(.vertical) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))], spacing: 20) {
+                    ForEach(symbols.filter { searchText.isEmpty ? true : $0.contains(searchText.lowercased()) }, id: \.hash) { icon in
+                        
                         Button {
-                            dismiss()
+                            self.selection = icon
                         } label: {
-                            Image(systemName: "xmark.circle.fill")
+                            Image(systemName: icon)
+                                .font(.system(size: 25))
+                                .foregroundColor(self.selection == icon ? Color.primary_color : Color.text_color)
                         }
-
-                    }
+                    }.padding(.top, 5)
                 }
-                .padding(.vertical, 5)
-                .tint(Color.gray)
-                
-            }.padding(.horizontal, 5)
+            }
+            .navigationTitle("icon_picker_title")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                    }
+                    
+                }
+            }
+            .tint(Color.gray)
         }
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
         .onChange(of: selection) { _, _ in
             dismiss()
         }
@@ -58,8 +54,6 @@ struct IconPickerModalView: View {
                 self.symbols = loadSymbols()
             }
         }
-        // TODO: Fix collapse
-        .searchable(text: $searchText)
     }
     
     private func loadSymbols() -> [String] {
